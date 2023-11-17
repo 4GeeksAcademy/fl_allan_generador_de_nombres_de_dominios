57ef842a583c518f81195ce2629ff9ea576e145e
Allan Roger <105656076+allanrogerfl@users.noreply.github.com>
2023-11-17 20:15:46 +0000

    primer_commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let p = 0; p < pronoun.length; ++p) {
+  for (let a = 0; a < adj.length; ++a) {
+    for (let n = 0; n < noun.length; ++n) {
+      console.log(pronoun[p] + adj[a] + noun[n] + ".com");
+    }
+  }
+}
