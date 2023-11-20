30f563ca57ccd63e166bc438ef8ff16a142b42cd
Allan Roger <105656076+allanrogerfl@users.noreply.github.com>
2023-11-20 20:24:31 +0000

    commit_MAP

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
+window.onload = () => {
+  let genereteDominios = (pronoun, adj, noun) => {
+    let dominos = [];
 
-for (let p = 0; p < pronoun.length; ++p) {
-  for (let a = 0; a < adj.length; ++a) {
-    for (let n = 0; n < noun.length; ++n) {
-      console.log(pronoun[p] + adj[a] + noun[n] + ".com");
+    for (let p = 0; p < pronoun.length; ++p) {
+      for (let a = 0; a < adj.length; ++a) {
+        for (let n = 0; n < noun.length; ++n) {
+          dominos.push(pronoun[p] + adj[a] + noun[n] + ".com");
+        }
+      }
     }
-  }
-}
+    return dominos;
+  };
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let result = genereteDominios(pronoun, adj, noun);
+  console.log(result);
+
+  document.querySelector("#nombres_dominios").innerHTML = result.map;
+};
